7e3b437c5bdb7df036e6a003802f8760daa72a91
rSosa-dev <RaulSG188@gmail.com>
2023-03-08 20:39:07 +0100

    Initial commit.

--- budget.py
+++ budget.py
@@ -1,4 +1,12 @@
 class Category:
 
-def create_spend_chart(categories):
-    aaa
+    def __init__(self, name):
+        self.name = name # It will allow the budget.Category("Food").
+        self.ledger = []
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description}) # Add the deposit to the list.
+
+
+#def create_spend_chart(categories):
+    
